ccd868edff79f7e7d41a5033b012ee79d90b7322
Asif_Nawaz <khogkhan949@gmail.com>
2023-10-22 01:42:11 -0400

    From Dollar.P  removed a few errors

--- Dollar.c
+++ Dollar.c
@@ -2,16 +2,16 @@
 int main()
 {
    float dollar, exchangeFee, dollarPerPound, totalAmountDollar;
-   float dollarPerYen;
+   float dollarPerYen; //changed data type
 
     printf("Enter the amount in Dollar:");
     scanf("%f",&dollar);
     exchangeFee = dollar * 0.1;
     totalAmountDollar = dollar - exchangeFee;
-    dollerPerPound = (totalAmountDollar / 2) * 0.79;
+    dollarPerPound = (totalAmountDollar / 2) * 0.79;
     dollarPerYen = (totalAmountDollar / 2) * 127.65;
-    printf("Half of the Dollar exchange to %2f GBP\n", dollarPerPoungccd);
-    printf("Half of the Dollar exchange to %d JPY\n", dollarPerYen);
+    printf("Half of the Dollar exchange to %2f GBP\n",dollarPerPound);  //Added .2 to format to 2 decimal places
+    printf("Half of the Dollar exchange to %2f JPY\n",dollarPerYen);   //changed format to specifier %2f
 
     return 0;
 }
